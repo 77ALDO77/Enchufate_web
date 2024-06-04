57689b987ecdcc452e0182cd99e33694a261499b
aldom <aldom@host.docker.internal>
2024-06-03 21:26:47 -0500

    arreglo pomp

--- src/main/java/com/mycompany/enchufateweb/JakartaRestConfiguration.java
+++ src/main/java/com/mycompany/enchufateweb/JakartaRestConfiguration.java
@@ -1,13 +1,13 @@
-package com.mycompany.enchufateweb;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-/**
- * Configures Jakarta RESTful Web Services for the application.
- * @author Juneau
- */
-@ApplicationPath("resources")
-public class JakartaRestConfiguration extends Application {
-    
-}
+//package com.mycompany.enchufateweb;
+//
+//import javax.ws.rs.ApplicationPath;
+//import javax.ws.rs.core.Application;
+//
+///**
+// * Configures Jakarta RESTful Web Services for the application.
+// * @author Juneau
+// */
+//@ApplicationPath("resources")
+//public class JakartaRestConfiguration extends Application {
+//    
+//}
